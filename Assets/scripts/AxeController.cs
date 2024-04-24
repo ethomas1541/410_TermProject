51a12a53731b4711cd416a1de514300f055e82c6
Hunter-McMahon85 <hmcmahon@uoregon.edu>
2024-04-24 15:40:45 -0700

    axe swing animated, dmg triggers next

---
+++ Assets/scripts/AxeController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AxeController : MonoBehaviour
+{
+    public Transform PlayerObject; // The object around which you want to rotate
+    public float rotationSpeed = 10f; // Speed of rotation
+    public float rotateDuration = 1f; // Duration for rotation
+    private bool isactive = false;
+    
+
+    public void OnAttack()
+    {
+        if (isactive == false)
+        {
+            StartCoroutine(RotateAndReturn());
+        }
+    }
+
+    IEnumerator RotateAndReturn()
+    {
+        isactive = true;
+        float elapsedTime = 0f;
+        while (elapsedTime < rotateDuration)
+        {
+            // Rotate around the target object
+            transform.RotateAround(PlayerObject.position, Vector3.up, rotationSpeed * Time.deltaTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+    
+        transform.Rotate(0.0f, 0.0f, 180.0f, Space.Self);
+        
+        elapsedTime = 0f;
+        while (elapsedTime < rotateDuration)
+        {
+            // Rotate around the target object
+            transform.RotateAround(PlayerObject.position, Vector3.up, -rotationSpeed * Time.deltaTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        } 
+
+         transform.rotation = Quaternion.Euler(0.0f, 90.0f, 180.0f);
+        isactive = false;
+    }
+}
