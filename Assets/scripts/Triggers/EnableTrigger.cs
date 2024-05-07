e18f01180679af0b16a881d68dea51fcf321e7d9
Isaac Perkins <isaacperkins987@gmail.com>
2024-05-07 11:39:57 -0700

    Started new wave spawner integration on tutorial level

---
+++ Assets/scripts/Triggers/EnableTrigger.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableTrigger : MonoBehaviour
+{
+    public List<GameObject> enables;
+    public List<string> activatedByTags;
+
+    void OnTriggerEnter(Collider other) {
+
+        // This object can trigger this event
+        if (activatedByTags.Contains(other.tag)) {
+
+            // Enable each enables object
+            foreach (GameObject t in enables) {
+                t.SetActive(true);
+            }
+        }
+    }
+}
