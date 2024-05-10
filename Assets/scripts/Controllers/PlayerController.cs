4f7a8880154b0e7052e8959fac3f06249cc8479a
Isaac Perkins <isaacperkins987@gmail.com>
2024-05-09 22:28:29 -0700

    Added left shift sprint

--- Assets/scripts/Controllers/PlayerController.cs
+++ Assets/scripts/Controllers/PlayerController.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5;
+    public float sprintFactor = 1.5f;
     public float jumpHeight = 2;
     public float groundedDistance = 0.5f;
 
@@ -16,6 +17,7 @@ public class PlayerController : MonoBehaviour
     private new Camera camera;
     private Vector2 direction;
     private float jumpVelocity;
+    private float sprintMultiplier;
 
     void Start()
     {
@@ -30,6 +32,8 @@ public class PlayerController : MonoBehaviour
 
         // We should only calculate this once for performance
         jumpVelocity = Mathf.Sqrt(2 * jumpHeight * Mathf.Abs(Physics.gravity.y));
+
+        sprintMultiplier = 1;
     }
 
     void FixedUpdate()
@@ -40,7 +44,7 @@ public class PlayerController : MonoBehaviour
 
     void MovePlayer()
     {
-        Vector3 movement = new Vector3(direction.x, 0, direction.y).normalized * speed;
+        Vector3 movement = new Vector3(direction.x, 0, direction.y).normalized * speed * sprintMultiplier;
 
         // Direction to animate based on player position
         Vector3 moveDirection = transform.InverseTransformDirection(movement).normalized;
@@ -97,7 +101,6 @@ public class PlayerController : MonoBehaviour
 
     public void TakeDamage() {
         animator.SetTrigger("Hit");
-
     }
 
     public void OnAttack(InputAction.CallbackContext context)
@@ -106,6 +109,14 @@ public class PlayerController : MonoBehaviour
         if (context.performed)
         {
             animator.SetTrigger("Attack");
+        }
+    }
+
+    public void OnSprint(InputAction.CallbackContext context) {
+        if (context.performed) {
+            sprintMultiplier = sprintFactor;
+        } else {
+            sprintMultiplier = 1;
         }
     }
 }
