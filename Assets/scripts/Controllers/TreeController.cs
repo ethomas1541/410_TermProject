cd926a7c4b33f69593a47dea9884a9b8b292998e
Isaac Perkins <isaacperkins987@gmail.com>
2024-05-12 15:04:35 -0700

    Tree respawn and stump working!

--- Assets/scripts/Controllers/TreeController.cs
+++ Assets/scripts/Controllers/TreeController.cs
@@ -13,24 +13,29 @@ public class TreeController : MonoBehaviour
 
     // let the tree access the players inventory
     public int WoodValue = 10;
-    //public GameObject Player;
+
+    // public GameObject Player;
     public WoodInventory Wallet;
-    //stump to replace with when choped down
+
+    // stump to replace with when choped down
     public GameObject Stump;
     public GameObject TreeModel;
-    public Transform TreeGFX;
+
+    private BoxCollider treeCollider;
 
     void Awake() {
+        treeCollider = GetComponent<BoxCollider>();
+
         // Subscribe to the OnDeath event
         healthController.OnDeath += OnDie;
-        // store default positions of gfx object
     }
 
     public void OnDie() {
         StartCoroutine(Kill());
         StartCoroutine(Respawn());
+
         // this is so the player cant wack the stump for infinite wood
-        healthController.Heal(5000);
+        treeCollider.enabled = false;
     }
 
     IEnumerator Kill()
@@ -39,7 +44,7 @@ public class TreeController : MonoBehaviour
 
         // Wait for the current transition to end
         yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).normalizedTime <= 1.0f );
-        
+
         Stump.SetActive(true);
         Wallet.AddWood(WoodValue);
 
@@ -49,17 +54,21 @@ public class TreeController : MonoBehaviour
     }
 
     IEnumerator Respawn()
-    {   
+    {
+        // This will play before it is active
+        animator.SetTrigger("Stand");
+
         //80 is final value for now
         //yield return new WaitForSeconds(80f);
+
         yield return new WaitForSeconds(5f);
+
         Stump.SetActive(false);
+
         // now set the health back to normal
         healthController.ResetHealth();
         TreeModel.SetActive(true);
-        
-        TreeGFX.position = new Vector3(0f, 0f, 0f); 
-        TreeGFX.rotation = Quaternion.Euler(0f, 0f, 0f); 
+        treeCollider.enabled = true;
 
     }
 }
