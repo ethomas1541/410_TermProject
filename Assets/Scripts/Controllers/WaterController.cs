112d776f970eba5ea8ed32234638fbf34709bdc6
Hunter-McMahon85 <hmcmahon@uoregon.edu>
2024-05-28 01:42:13 -0700

    added the rivers to Lv2 & LV3

---
+++ Assets/Scripts/Controllers/WaterController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterController : MonoBehaviour
+{   
+    // The amount of force to apply to the player
+    public float riverForce = 10f;
+    public Vector3 forceDirection;
+
+    private void Start()
+    {
+        forceDirection = transform.right; 
+    }
+    
+
+    private void OnTriggerStay(Collider other)
+    {
+        
+        // Apply force to the player in the calculated direction
+        Rigidbody playerRigidbody = other.GetComponent<Rigidbody>();
+        playerRigidbody.AddForce(forceDirection * riverForce, ForceMode.Force);
+    }          
+}
