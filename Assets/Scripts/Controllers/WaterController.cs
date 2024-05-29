570ac1a7f1e260073f8828ad50dfedb1b1a28739
Isaac Perkins <isaacperkins987@gmail.com>
2024-05-28 23:30:35 -0700

    Get rid of all these warnings

--- Assets/Scripts/Controllers/WaterController.cs
+++ Assets/Scripts/Controllers/WaterController.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WaterController : MonoBehaviour
-{   
+{
     // The amount of force to apply to the player
     public float riverForce = 10f;
     public Vector3 forceDirection;
 
     private void Start()
     {
-        forceDirection = transform.right; 
+        forceDirection = transform.right;
     }
-    
+
 
     private void OnTriggerStay(Collider other)
     {
-        
-        // Apply force to the player in the calculated direction
-        Rigidbody playerRigidbody = other.GetComponent<Rigidbody>();
-        playerRigidbody.AddForce(forceDirection * riverForce, ForceMode.Force);
-    }          
+        if (other.tag == "Player" || other.tag == "Enemy" || other.tag == "Poacher") {
+            // Apply force to the player in the calculated direction
+            Rigidbody playerRigidbody = other.GetComponent<Rigidbody>();
+            playerRigidbody.AddForce(forceDirection * riverForce, ForceMode.Force);
+        }
+    }
 }
